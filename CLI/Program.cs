ac4c5f9cc933d7ff62a1ba72ed628d25cf7bdfde
Daniel Shroff <danielshroff@hotmail.com>
2018-09-20 11:17:41 +0530

    Added functionality to standard cli

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -9,8 +9,8 @@ namespace CLI
         {
             Console.WriteLine("ASMBLR Core CLI, using ASMBLR version "+Interpreter.ver);
             var interpreter = new Interpreter();
-            Console.WriteLine("Update/repair(y, n)");
-            if(Console.ReadKey().KeyChar.Equals('y')){using (var client = new WebClient())
+            Console.WriteLine("Update/Repair (y, n)");
+            if(Console.ReadLine().Equals("y")){using (var client = new WebClient())
             {
                 client.DownloadFile("https://github.com/danielandastro/ASMBLR/blob/update-branch/core.dll?raw=true", "ASMBLR.dll");
             }}
--- Standard Interpreter CLI/Program.cs
+++ Standard Interpreter CLI/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using ASMBLR;
-
+using System.Net;
 namespace Standard_Interpreter_CLI
 {
     internal class Program
@@ -9,6 +9,10 @@ namespace Standard_Interpreter_CLI
         {
             var interpreter = new Interpreter();
             Console.WriteLine("ASMBLR Standard CLI, using AMBLR version "+Interpreter.ver);
+            if(Console.ReadKey().KeyChar.Equals('y')){using (var client = new WebClient())
+            {
+                client.DownloadFile("https://github.com/danielandastro/ASMBLR/blob/update-branch/stnd.dll?raw=true", "ASMBLR.dll");
+            }}
             while (true)
             {
                 Console.Write(">");
