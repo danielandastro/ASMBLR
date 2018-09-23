30005f889c6c82a102de83b6c326a3237cbb02ab
Daniel Shroff <danielshroff@hotmail.com>
2018-09-23 15:40:01 +0530

    Added auto update version checking and refactored starting calls etc. to mehtod preflightchecks, as it was getting too big

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -9,21 +9,62 @@ namespace CLI
         {
             Console.WriteLine("ASMBLR Core CLI, using ASMBLR version "+Interpreter.ver);
             var interpreter = new Interpreter();
-            Console.WriteLine("Update(y, n)");
-            if(Console.ReadLine().Equals("y")){try{using (var client = new WebClient())
-            {
-                client.DownloadFile("https://github.com/danielandastro/ASMBLR/blob/update-branch/core.dll?raw=true", "ASMBLR.dll");
-            }
-                
-            }
-                catch(Exception){Console.WriteLine("Connection Failed");}
-            }
+            PreFlightChecks();
+
+
             while (true)
             {
                 Console.Write(">");
                 var hold = Console.ReadLine();
                 hold =interpreter.Runner(hold);
                 Console.WriteLine(hold);
+            }
+        }
+
+        private static void PreFlightChecks()
+        {
+            var client = new WebClient();
+            var newVer = "";
+            try
+            {
+                newVer = client.DownloadString(
+                    "https://raw.githubusercontent.com/danielandastro/ASMBLR/update-branch/update.txt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Connection Failed");
+            }
+
+            if (!newVer.Contains(Interpreter.ver))
+            {
+                Console.WriteLine("Update Available " + newVer + "Show ChangeLog");
+                if (Console.ReadLine().Equals("y"))
+                {
+                    
+                    try
+                    {
+                        Console.WriteLine(client.DownloadString("https://raw.githubusercontent.com/danielandastro/ASMBLR/update-branch/chnglg.txt"));
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Connection Failed");
+                    }
+
+                    Console.WriteLine("Update(y/n)");
+                    if (Console.ReadLine().Equals("y"))
+                    {
+                        try
+                        {
+                            client.DownloadFile("https://github.com/danielandastro/ASMBLR/blob/update-branch/core.dll?raw=true",
+                                "ASMBLR.dll");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Connection Failed");
+                        }
+                        Console.WriteLine("ASMBLR Core CLI, using ASMBLR version "+Interpreter.ver);
+                    }
+                }
             }
         }
     }
--- Standard Interpreter CLI/Program.cs
+++ Standard Interpreter CLI/Program.cs
@@ -8,22 +8,84 @@ namespace Standard_Interpreter_CLI
         public static void Main(string[] args)
         {
             var interpreter = new Interpreter();
-            Console.WriteLine("ASMBLR Standard CLI, using ASMBLR version "+Interpreter.ver);
+            PreFlightChecks();
+            ;
+            Console.WriteLine("ASMBLR Standard CLI, using ASMBLR version " + Interpreter.ver);
             Console.WriteLine("Update(y, n)");
-            if(Console.ReadLine().Equals('y')){try{using (var client = new WebClient())
+            if (Console.ReadLine().Equals('y'))
             {
-                client.DownloadFile("https://github.com/danielandastro/ASMBLR/blob/update-branch/stnd.dll?raw=true", "ASMBLR.dll");
-            }
-                    
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(
+                            "https://github.com/danielandastro/ASMBLR/blob/update-branch/stnd.dll?raw=true",
+                            "ASMBLR.dll");
+                    }
+
                 }
-                catch(Exception){Console.WriteLine("Connection Failed");}
+                catch (Exception)
+                {
+                    Console.WriteLine("Connection Failed");
+                }
             }
+
             while (true)
             {
                 Console.Write(">");
                 var hold = Console.ReadLine();
-                hold =interpreter.Runner(hold);
+                hold = interpreter.Runner(hold);
                 Console.WriteLine(hold);
+            }
+        }
+
+        private static void PreFlightChecks()
+        {
+            var client = new WebClient();
+            var newVer = "";
+            try
+            {
+                newVer = client.DownloadString(
+                    "https://raw.githubusercontent.com/danielandastro/ASMBLR/update-branch/update.txt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Connection Failed");
+            }
+
+            if (!newVer.Contains(Interpreter.ver))
+            {
+                Console.WriteLine("Update Available " + newVer + "Show ChangeLog");
+                if (Console.ReadLine().Equals("y"))
+                {
+
+                    try
+                    {
+                        Console.WriteLine(client.DownloadString(
+                            "https://raw.githubusercontent.com/danielandastro/ASMBLR/update-branch/chnglg.txt"));
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Connection Failed");
+                    }
+
+                    Console.WriteLine("Update(y/n)");
+                    if (Console.ReadLine().Equals("y"))
+                    {
+                        try
+                        {
+                            client.DownloadFile(
+                                "https://github.com/danielandastro/ASMBLR/blob/update-branch/core.dll?raw=true",
+                                "ASMBLR.dll");
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Connection Failed");
+                        }
+
+                        Console.WriteLine("ASMBLR Core CLI, using ASMBLR version " + Interpreter.ver);
+                    }
+                }
             }
         }
     }
